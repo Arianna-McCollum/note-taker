4076dcae5ec14b1e9a8f4a0bed540f9ebd1f9d3f
Electrolion <AMccollum71@gmail.com>
2021-09-03 17:58:07 -0400

    cleared db.json + updated server.js

--- server.js
+++ server.js
@@ -5,32 +5,71 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+const allNotes = require('./db/db.json');
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-const allNotes = require('./db/db');
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 function createNewNote(body, notesArray) {
     const newNote = body;
+    if (!Array.isArray(notesArray))
+        notesArray = [];
+    
+    if (notesArray.length === 0)
+        notesArray.push(0);
+
+    body.id = notesArray[0];
+    notesArray[0]++;
+
     notesArray.push(newNote);
     fs.writeFileSync(
         path.join(__dirname, './db/db.json'),
-        JSON.stringify({ allNotes: notesArray }, null, 2)
+        JSON.stringify(notesArray, null, 2)
     );
     return newNote;
 }
 
-app.get('/api/notes', (req, res) => {
-    res.json(allNotes);
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNote(req.body, allNotes);
+    res.json(newNote);
 });
 
-app.post('/api/notes', (req, res) => {
-    req.body.id = allNotes.length.toString();
+function deleteNote(id, notesArray) {
+    for (let i = 0; i < notesArray.length; i++) {
+        let note = notesArray[i];
 
-    const newNote = createNewNote(req.body, allNotes);
+        if (note.id == id) {
+            notesArray.splice(i, 1);
+            fs.writeFileSync(
+                path.join(__dirname, './db/db.json'),
+                JSON.stringify(notesArray, null, 2)
+            );
 
+            break;
+        }
+    }
+}
 
-    res.json(newNote);
+app.delete('/api/notes/:id', (req, res) => {
+    deleteNote(req.params.id, allNotes);
+    res.json(true);
 });
 
 app.listen(PORT, () => {
